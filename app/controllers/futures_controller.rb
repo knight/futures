898680af53526eee3eb394a6188cbbddb6fc015e
najcik <knight@kopernet.org>
2012-02-25 21:54:17 +0100

    few tests and implementation of creating entries (from scratch)

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -2,4 +2,20 @@ class FuturesController < ApplicationController
   def index
     @futures = Future.all
   end
+  
+  def create
+    future = Future.new(params[:future])
+    
+    #respond_to do |format|
+      if future.save
+        redirect_to :futures
+      else
+        redirect_to :new_future
+      end
+    #end
+
+  end
+  def new
+    @future = Future.new
+  end
 end
--- spec/controllers/futures_controller_spec.rb
+++ spec/controllers/futures_controller_spec.rb
@@ -10,4 +10,33 @@ describe FuturesController do
     get :index
     assigns(:futures).should eq(Future.all)
   end
+  
+  it "should have new action" do
+    get :new
+    response.code.should eq "200"
+  end
+  
+  it "should assign a blueprint in the new action" do
+    get :new
+    assigns(:future).should be_an(Future)
+  end
+  
+  it "should redirect back to new action if the data are invalid" do
+    post :create, {}
+    response.should redirect_to(:new_future)
+    Future.all.count.should == 0 
+  end
+  
+  it "should redirect back to index action if the creation is successful" do
+    post :create, { :future=>{
+      :ticker=>"FW20",
+      :dyyyymmdd=>"20120224",
+      :open=>"2325",
+      :high=>"2335",
+      :low=>"2311",
+      :close=>"2323"
+    }}
+    response.should redirect_to(:futures)
+    Future.all.count.should == 1
+  end
 end
