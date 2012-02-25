f6997ef4447b43984f0c62083330b1035f1ac792
najcik <knight@kopernet.org>
2012-02-25 11:08:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futures::Application.config.secret_token = 'a6a9ef7702c7ab37b2283136fb30e9105ee4882bc778be54cca5fa2cee247bd6ed58c16be2d0a5ea4d3c9feec1ccbac876958bbb3aaaf41ffbc0f8bf87c5a1b8'
