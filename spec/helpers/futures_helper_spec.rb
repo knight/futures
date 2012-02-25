115b534cd0b041933e5f060fb5d63f25f15f2800
najcik <knight@kopernet.org>
2012-02-25 20:27:35 +0100

    disabled helper spec

--- spec/helpers/futures_helper_spec.rb
+++ spec/helpers/futures_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe FuturesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe FuturesHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
