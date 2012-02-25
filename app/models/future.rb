69ad5525be24a1906eff7c3c3566543edbf1166e
najcik <knight@kopernet.org>
2012-02-25 15:30:40 +0100

    few more tests to ensure data consistency

--- app/models/future.rb
+++ app/models/future.rb
@@ -1,2 +1,4 @@
 class Future < ActiveRecord::Base
+  validates_presence_of :ticker, :dyyyymmdd, :open, :high, :low, :close
+  validates_uniqueness_of :dyyyymmdd
 end
--- spec/models/future_spec.rb
+++ spec/models/future_spec.rb
@@ -9,4 +9,14 @@ describe Future do
     Future.create!(:ticker=>"FW20", :dyyyymmdd=>20120210, :open=>2372, :high=>2376, :low=>2344, :close=>2346, :vol=>37728, :openint=>37728)
     Future.all.count.should == 1
   end
+  it "should not save if mandatory attiributes are missing" do
+    future = Future.create(:ticker=>"FW20")
+    future.should_not be_valid
+  end
+  
+  it "should have unique dyyyymmdd attribute" do
+     Future.create!(:ticker=>"FW20", :dyyyymmdd=>20120210, :open=>2372, :high=>2376, :low=>2344, :close=>2346, :vol=>37728, :openint=>37728)
+     future = Future.create(:ticker=>"FW20", :dyyyymmdd=>20120210, :open=>2372, :high=>2376, :low=>2344, :close=>2346, :vol=>37728, :openint=>37728)
+     future.should_not be_valid
+  end
 end
