420ccdd4997948848e521d7b0e059f4ccd733b99
najcik <knight@kopernet.org>
2012-02-26 13:46:29 +0100

    split specs

--- spec/requests/views_spec.rb
+++ spec/requests/views_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
+
 describe "Futures views" do
-  it "should render form when visiting new action" do
-    visit '/futures/new'
-    page.should have_selector 'form'
+  describe "new action" do
+    it "should render form when visiting new action" do
+      visit '/futures/new'
+      page.should have_selector 'form'
+    end
+  end
+  
+  describe "index action" do
+    it "should render a table with futures quotes" do
+      visit '/futures/'
+      page.should have_selector 'table'
+    end
+    it "should render a link to new action" do
+      visit '/'
+      page.should have_selector 'a[href="/futures/new"]'
+    end
   end
 end
