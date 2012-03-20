7a389f90045444d1aa244fd1053c16c2176b325e
najcik <knight@kopernet.org>
2012-03-20 22:59:04 +0100

    playing with webrat assertions

--- spec/views/quotes/new.erb_spec.rb
+++ spec/views/quotes/new.erb_spec.rb
@@ -1,10 +1,23 @@
 require "spec_helper"
 
 describe "quotes/new" do
-  let(:quote) { stub_model(Quote) }
+  context "quote creation" do
+  let(:quote) { stub_model(Quote).as_new_record }
   it "should have a form to send new data" do
     assign(:quote, quote)
     render
-    rendered.should have_selector('form')
+    rendered.should have_selector('form[action="/quotes"][method="post"]')
+   
+  end
+  end
+  context "quote edition" do
+    let(:quote) { stub_model(Quote, {:id=>1}) }
+    it "can be used for quote editing" do
+      assign(:quote, quote)
+      render
+      rendered.should have_selector('form[action="/quotes/1"]') do |f|
+        f.should have_selector('input[type="hidden"][value="put"]')
+      end
+    end
   end
 end
